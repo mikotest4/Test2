32b327f811372f587b66249ce710cb21ac2bf739
mikotest4 <fakealok756@gmail.com>
2025-06-20 13:48:01 +0530

    Update check_user_status.py

--- plugins/check_user_status.py
+++ plugins/check_user_status.py
@@ -11,6 +11,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact @hero_botss  😝", quote=True)
+            await cmd.reply_text("ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ʙʏ ᴏᴡɴᴇʀ ғᴏʀ ᴜɴʙᴀɴ ᴄᴏʙᴀɴ ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ ʜᴇʀᴇ", quote=True)
             return
     await cmd.continue_propagation()
